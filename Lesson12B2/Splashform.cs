33cf4f7b2900be6159f9cf137b448935690685ad
Eva <Eva@DESKTOP-CDRCGMN>
2017-08-08 16:45:22 -0400

    Made splash form

---
+++ Lesson12B2/Splashform.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Name: Wayne Pud
+ * Student ID: 300931522
+ * Date: August 8th, 2017
+ * Description: This is a splashform
+ * Version: 0.1 - Created the Project
+ */
+
+namespace Lesson12B2
+{
+    public partial class Splashform : Form
+    {
+        public Splashform()
+        {
+            InitializeComponent();
+        }
+    }
+}
